e7c0317b275e51e94b2a6895c5f5bf2b2d8b4aa5
4lessandrodev <alessandroadm@live.com>
2021-09-07 21:05:19 -0300

    fix(template): added input type to template

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,8 +1,14 @@
-import env from 'env-var';
+import * as env from 'env-var';
 const envVar = env.from(process.env, {}, env.logger);
 
 export const JWT_EXPIRATION_IN_HOURS = envVar
 	.get('JWT_EXPIRATION_IN_HOURS')
-	.required()
 	.default(2)
+	.required()
 	.asIntPositive();
+
+export const JWT_SECRET = envVar
+	.get('JWT_SECRET')
+	.default('d0495cd085f1702f01b8fd32853')
+	.required()
+	.asString();
